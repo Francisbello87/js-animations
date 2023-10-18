bf46237e32a638e5fbd25b94cf5105d2b784b6a3
Francis Bello <bellofrancis87@gmail.com>
2023-10-18 09:59:21 +0100

    Chapter 15-Friction

--- src/HTML/15-Friction/main.js
+++ src/HTML/15-Friction/main.js
@@ -6,21 +6,26 @@ class Rectangle {
     height = 10,
     velocityX = 0,
     velocityY = 0,
+    friction = 1,
     gravity = 0,
     color = "black",
   }) {
-    this.color = color;
+    this.gravity = gravity;
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
     this.velocityX = velocityX;
     this.velocityY = velocityY;
-    this.gravity = gravity;
+    this.color = color;
+    this.friction = friction;
   }
 
   update(deltaTimeFrameFromPreviousExecution) {
-    this.velocityY += GRAVITY * deltaTimeFrameFromPreviousExecution;
+    this.velocityX *= this.friction;
+
+    this.velocityY += this.gravity * deltaTimeFrameFromPreviousExecution;
+
     this.x += this.velocityX * deltaTimeFrameFromPreviousExecution;
     this.y += this.velocityY * deltaTimeFrameFromPreviousExecution;
   }
@@ -31,11 +36,11 @@ class Rectangle {
   }
 }
 
+// ------- Initialization -------
 const CANVAS_WIDTH = 450;
 const CANVAS_HEIGHT = 450;
 
 const canvas = document.getElementById("canvas");
-const logger = document.getElementById("logger");
 
 const dpr = devicePixelRatio;
 canvas.width = CANVAS_WIDTH * devicePixelRatio;
@@ -46,48 +51,38 @@ canvas.style.setProperty("height", CANVAS_HEIGHT + "px");
 
 const ctx = canvas.getContext("2d");
 
-const GRAVITY = 70;
-// const RECTANGLES_COUNT = 50;
+const GRAVITY = 160;
 const rectangles = [];
-setInterval(function() {
-  const rectangle = new Rectangle({
-    x: 0,
-    y: 100,
-    width: 20,
-    height: 20,
-    velocityX: Math.random() * 200 + 200,
-    velocityY: Math.random() * 100 + 100,
-    gravity:GRAVITY,
-    color:getRandomRGBColor()
-  });
-  rectangles.push(rectangle)
-}, 2000)
 
-for (let i = 0; i < RECTANGLES_COUNT; i++) {
-  const radius = Math.random() * 20;
-  rectangles.push(
-    new Rectangle({
-      gravity: GRAVITY,
-      x: canvas.width / 2,
-      y: canvas.height,
-      width: radius,
-      height: radius,
-      velocityX: 20 + (Math.random() * 2 - 1) * 300,
-      velocityY: -Math.random() * 300 + 50,
-      color: getRandomRGBColor(),
-    })
-  );
-}
+spawnRect();
+setInterval(spawnRect, 2000);
 
 let oldTime = 0;
 
 requestAnimationFrame(drawFrame);
 
+function spawnRect() {
+  const rectangle = new Rectangle({
+    x: 0,
+    y: 100,
+    width: 20,
+    height: 20,
+    velocityX: Math.random() * 500 + 500,
+    velocityY: Math.random() * 200 + 200,
+    friction: 0.99,
+    gravity: GRAVITY,
+    color: getRandomRGBColor(),
+  });
+  rectangles.push(rectangle);
+}
+
+// ------- Render Loop -------
 function drawFrame(ts) {
   ts /= 1000;
   const deltaTimeFrameFromPreviousExecution = ts - oldTime;
   oldTime = ts;
 
+  // clear our canvas contents
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   let i = rectangles.length;
@@ -95,25 +90,33 @@ function drawFrame(ts) {
     const rect = rectangles[i];
     rect.update(deltaTimeFrameFromPreviousExecution);
 
-    if (rect.y > canvas.height) {
-      rect.x = canvas.width / 2;
-      rect.y = canvas.height;
-      (rect.velocityX = 20 + (Math.random() * 2 - 1) * 100),
-        (rect.velocityY = -Math.random() * 225 + 50);
-    }
-    if (rect.x > canvas.width - rect.width || rect.x < 0) {
-      rect.velocityX *= -1;
-    }
+    checkBoundaries(rect);
+
     rect.render(ctx);
   }
 
   requestAnimationFrame(drawFrame);
 }
 
+function checkBoundaries(rect) {
+  if (rect.x > canvas.width - rect.width) {
+    rect.velocityX *= -1;
+  } else if (rect.x < 0) {
+    rect.velocityX *= -1;
+  }
+
+  if (rect.y > canvas.height - rect.height) {
+    rect.velocityY *= -1;
+    rect.y = canvas.height - rect.height;
+    rect.velocityY *= 0.5;
+  } else if (rect.y < 0) {
+    rect.velocityY *= -1;
+  }
+}
+
 function getRandomRGBColor() {
   const randomR = Math.random() * 255;
   const randomG = Math.random() * 255;
   const randomB = Math.random() * 255;
-
   return `rgba(${randomR}, ${randomG}, ${randomB})`;
 }
